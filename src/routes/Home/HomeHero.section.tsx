ef966cd139ea280d4cac84ead08890c4d09c0323
KKKircheff <kircheff@protonmail.com>
2024-09-29 14:35:10 +0300

    image masked component created

--- src/routes/Home/HomeHero.section.tsx
+++ src/routes/Home/HomeHero.section.tsx
@@ -5,7 +5,7 @@ import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 import { scroller } from 'react-scroll'
 import TextContent from '../../components/typography/TextContent.component';
 import HeroTitle from '../../components/typography/HeroTitle.component';
-
+import ImageWithMask from '../../components/ImageWithMask';
 
 const scrollToCatalogues = () => {
     scroller.scrollTo('catalogues', {
@@ -26,57 +26,60 @@ const HomeHero = () => {
     const r = useTheme().radius;
 
     return (
-        <Grid
-            container spacing={{ xs: 0, md: 6 }}
-            pt={{ xs: 0, md: 2 }}
-            sx={{ flexGrow: 1 }}
-            justifyContent='space-between'
-            alignItems='center'>
-            <Grid xs={12} md={7} order={{ xs: 1, md: 0 }} py={6}>
-                <Stack
-                    direction='column'
-                    justifyContent='center'
-                // borderLeft={{ md: `1.3px solid ${c.neutral[400]}` }}
-                // pl={{ xs: 0, md: 5 }}
-                >
-                    <HeroTitle
-                        pb={{ xs: 5, md: 4, lg: 4 }}
-                    >Сертифицирани семена за картофи от Нидерландия
-                    </HeroTitle>
-                    <TextContent
-                        pb={{ xs: 2, md: 2, lg: 2 }}
-                    >Лицензиран вносител на седем ранни, средно ранни и късни картофени семена
-                    </TextContent>
-                    <TextContent
-                        pb={{ xs: 8, md: 6, lg: 8 }}
-                    >Вносител на висококачествени картофени семена със свободен лиценз
-                    </TextContent>
-                    <Stack direction={{ xs: 'column', sm: 'row' }} gap={2}>
-                        <BlackButton
-                            size='lg'
-                            color='neutral'
-                            onClick={scrollToCatalogues}
-                            endDecorator={<ArrowOutwardIcon />}>
-                            <Typography textColor='inherit' fontSize='inherit'>Семена</Typography>
-                        </BlackButton>
-                        <BlackButton
-                            size='lg'
-                            onClick={scrollToContact}
-                            endDecorator={<ArrowOutwardIcon />}>
-                            <Typography textColor='inherit' fontSize='inherit'>Контакт</Typography>
-                        </BlackButton>
+        <Stack direction={'column'}>
+            <ImageWithMask width={'100%'} imgSrc={'/images/hero.webp'} maskSrc={'/images/home-hero-mask.png'} alt={'SVG image'} aspectRatio={1280 / 850} />
+            <Grid
+                container spacing={{ xs: 0, md: 6 }}
+                pt={{ xs: 0, md: 2 }}
+                sx={{ flexGrow: 1 }}
+                justifyContent='space-between'
+                alignItems='center'>
+                <Grid xs={12} md={7} order={{ xs: 1, md: 0 }} py={6}>
+                    <Stack
+                        direction='column'
+                        justifyContent='center'
+                    // borderLeft={{ md: `1.3px solid ${c.neutral[400]}` }}
+                    // pl={{ xs: 0, md: 5 }}
+                    >
+                        <HeroTitle
+                            pb={{ xs: 5, md: 4, lg: 4 }}
+                        >Сертифицирани семена за картофи от Нидерландия
+                        </HeroTitle>
+                        <TextContent
+                            pb={{ xs: 2, md: 2, lg: 2 }}
+                        >Лицензиран вносител на седем ранни, средно ранни и късни картофени семена
+                        </TextContent>
+                        <TextContent
+                            pb={{ xs: 8, md: 6, lg: 8 }}
+                        >Вносител на висококачествени картофени семена със свободен лиценз
+                        </TextContent>
+                        <Stack direction={{ xs: 'column', sm: 'row' }} gap={2}>
+                            <BlackButton
+                                size='lg'
+                                color='neutral'
+                                onClick={scrollToCatalogues}
+                                endDecorator={<ArrowOutwardIcon />}>
+                                <Typography textColor='inherit' fontSize='inherit'>Семена</Typography>
+                            </BlackButton>
+                            <BlackButton
+                                size='lg'
+                                onClick={scrollToContact}
+                                endDecorator={<ArrowOutwardIcon />}>
+                                <Typography textColor='inherit' fontSize='inherit'>Контакт</Typography>
+                            </BlackButton>
+                        </Stack>
                     </Stack>
-                </Stack>
+                </Grid>
+                <Grid xs={12} md={5} lg={5} >
+                    <AspectRatio
+                        ratio='1.3'
+                        sx={{ borderRadius: r.md }}
+                    >
+                        <img src={HeroImg} style={{ width: '100%', height: '100%' }} loading='eager' alt='Home kitchen interior graphics' />
+                    </AspectRatio>
+                </Grid>
             </Grid>
-            <Grid xs={12} md={5} lg={5} >
-                <AspectRatio
-                    ratio='1.3'
-                    sx={{ borderRadius: r.md }}
-                >
-                    <img src={HeroImg} style={{ width: '100%', height: '100%' }} loading='eager' alt='Home kitchen interior graphics' />
-                </AspectRatio>
-            </Grid>
-        </Grid>
+        </Stack>
     )
 }
 
