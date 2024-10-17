1c18993d28cd6430db5c49a9e1ea7e621e5b50f2
KKKircheff <kircheff@protonmail.com>
2024-10-18 00:02:29 +0300

    vite.config automate build & sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig, loadEnv} from 'vite';
+import react from '@vitejs/plugin-react';
+import Sitemap from 'vite-plugin-sitemap';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({command, mode}) => {
+    const paths = ['varieties', 'contact'];
+    const dynamicRoutes = paths.map((path) => `/${path}`);
+
+    return {
+        plugins: [
+            react(),
+            Sitemap({
+                dynamicRoutes,
+                hostname: 'https://agrimatic.bg',
+                robots: [
+                    {
+                        userAgent: '*',
+                        allow: '/',
+                    },
+                ],
+            }),
+        ],
+        build: {
+            minify: mode === 'production' ? 'terser' : false,
+            sourcemap: mode === 'development', // disable sourcemap for production
+            manifest: true,
+
+            rollupOptions: {
+                output: {
+                    manualChunks(id: string) {
+                        if (id.includes('@open-ish') || id.includes('tslib')) {
+                            return '@open-ish';
+                        }
+                        if (
+                            id.includes('react-router-dom') ||
+                            id.includes('@remix-run') ||
+                            id.includes('react-router')
+                        ) {
+                            return '@react-router';
+                        }
+                    },
+                },
+            },
+        },
+    };
+});
